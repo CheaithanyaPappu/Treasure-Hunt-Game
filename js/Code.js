44efa34a1470428e0ab7d948afa907c8ca2c320b
CheaithanyaPappu <73804385+CheaithanyaPappu@users.noreply.github.com>
2021-07-18 12:51:12 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill ("white");
+    textSize (15);
+    text( " C U N F I T N O ", 700,150)
+    fill ("lightblue");
+    text ("Hint: Group of statements together perform one task",700,170 );
+
+    fill ("white");
+    textSize(15)
+    text ("A T A B S A E",100,250);
+    fill ("lightblue");
+    text("Hint: Organized Data" ,100,270 );
+// add code for displaying rest of the names and the hints.
+    
+
+}
